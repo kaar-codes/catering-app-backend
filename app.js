f2b983d7fc587e952587ae343259e06c022f6268
kaar-codes <webdesignkaar@gmail.com>
2026-03-08 14:26:23 +0530

    CORS updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
 import menuRouter from "./routes/menuRoute.js";
@@ -7,9 +8,15 @@ import { authenticateUser } from "./controllers/authController.js";
 
 const app = express();
 
+app.use(cors());
+
 // Parser for JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/testRoute", async (req, res) => {
+  res.status(202).json({ message: "Test Route" });
+});
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
