a37013bc2e913741137561865bc70cb4fbdf20cb
kaar-codes <webdesignkaar@gmail.com>
2026-03-17 22:00:40 +0530

    Login Updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import { compare } from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel";
 
 export async function authenticateUser(req, res, next) {
   try {
@@ -11,5 +14,40 @@ export async function authenticateUser(req, res, next) {
     }
   } catch (error) {
     res.status(401).json({ message: error.message });
+  }
+}
+
+export async function loginUser(req, res) {
+  try {
+    const { email, phone, password: plainPassword } = req.body;
+    const { _id: userId, password: hashPassword } = await User.findOne({
+      $or: [{ email, phone }],
+    });
+    await compare(plainPassword, hashPassword);
+
+    const accessToken = jwt.sign(
+      { userId, email },
+      process.env.JWT_SECRET_KEY,
+      { algorithm: "HS256", expiresIn: process.env.JWT_EXPIRES_IN },
+    );
+    const refreshToken = jwt.sign(
+      { userId },
+      process.env.JWT_REFRESH_SECRET_KEY,
+      { algorithm: "HS256", expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRY },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: process.env.JWT_REFRESH_TOKEN_EXPIRY,
+    });
+
+    res.status(200).json({
+      message: "User Authenticated",
+      token: accessToken,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Bad Authentication" });
   }
 }
